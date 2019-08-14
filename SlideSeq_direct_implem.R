f32f430baa534518b0e1d920400fc3d3013ea0e4
moiraek <moiraek@hotmail.se>
2019-08-14 10:50:32 +0200

    Obsolete, first direct implementation

--- SlideSeq_direct_implem.R
+++ SlideSeq_direct_implem.R
@@ -1,33 +1,34 @@
-#!/bin/Rscript
+#!/usr/bin/Rscript
 #-----------------------------------------------------------------------
 #
 # Moira Ek
 # 2019 07 12
-# Försök att implementera SlideSeq-DGE-metoden
-# 1. Direkt, utan att ännu ha gjort några anpassningar till ST,
-#    jobbar med råa countdata initialt. Använder spotkoordinater för
-#    avstånd.
+# Attempt to implement the SlideSeq DGE method.
+# 1. Direct, without any adaptations to ST, initially working with
+#    raw count data. Using spot coordinates for distance calculations.
 # 
 #-----------------------------------------------------------------------
 
+#----------------------------------------------------------------------
+# Preparations, one sample is analyzed at a time. Initially looking
+#  at a very small number of genes, due to long computation times
+#----------------------------------------------------------------------
 setwd("/home/moiraek/summerp19/SlideSeq_etc/Till_git")
-library(STutility)
-library(Seurat)
 
 data <- as.data.frame(t(read.table("Rep1_MOB_count_matrix-1.tsv", check.names=FALSE)))
 
 testdata <- data[1:10,]
 #--------------------------------------------------------------------
-# Funktioner:
+# Functions:
 #--------------------------------------------------------------------
 
 
-# Skapa funktion för att ge en slumpmässig uppsättning spots, med
-#  SL enligt mängden transkript de fångat. Utan återläggning.
-#  STdata är en dataframe med spots som kolumner, gener som rader 
-#  (rådatan), nr är antalet spots som önskas. Nr kan aldrig vara
-#  större än det totala antalet spots i den tänkta tillämpningen
-#  --> behöver ej lägga in en if-sats.
+# Create a function to give a random set of spots, with probabilities
+#  according to the number of transcripts at each spot. Without
+#  replacement. STdata is a dataframe with spots as columns, genes as
+#  rows (the raw data), nr is the desired number of spots. Nr can never
+#  exceed the total number of spots in the intended application --> no
+#  if statement is needed.
 
 SpotProb <- function(STdata, nr){
   tot_spot <- colSums(STdata)
@@ -41,12 +42,12 @@ SpotProb <- function(STdata, nr){
 }
 
 
-# Funktion för att ge en fördelning av de Euklidiska avstånden mellan
-#  en given uppsättning spots, spot_coords är en vektor av spotnamn 
-#  på formen "x-koordinat x y-koordinat". För varje spot beräknas 
-#  det euklidiska avståndet till alla spots till höger om den (--> 
-#  avståndet för varje par av spots tas med en gång). 
-#  Output: avstånden. 
+# Function to give a distribution of the pairwise Euclidean distances 
+#  between a given set of spots, spot_coords is a vector of spot names
+#  of the form "x coordinate x y coordinate". For each spot the 
+#  Euclidean distance to all spots to its right in the vector is 
+#  calculated (--> the distance for each pair of spots is only included
+#  once). Output: the distances.
 
 distrib <- function(spot_coords){
   len <- 0
@@ -71,32 +72,31 @@ distrib <- function(spot_coords){
 
 
 #--------------------------------------------------------------------
-# Försök
+# Attempt
 #--------------------------------------------------------------------
 
-# Beräkna det maximala avståndet mellan inkluderade spots, för att
-#  sätta upp lämpliga breakpoints för hist. Lät tidigare den verkliga
-#  fördelningen sätta bingränserna (gav antal bins för den, tog sedan
-#  $breaks som input till hist för de slumpade spotuppsättningarna), 
-#  detta ger dock risken att alla avståndsvärden i de slumpade inte
-#  täcks in, tex om en gen uttrycks i en begränsad region. (De
-#  spotsen dras ju från hela arrayen, efter mängden fångade 
-#  transkript.)
-# NB - se mer på vad som är ett lämpligt binantal.
+# Calculate the maximum distance between included spots, in order to
+#  set appropriate breakpoints for hist. Before, the true distribution 
+#  was used to set the bin breakpoints (set a number of bins for it,
+#  used $breaks as input to hist for the random distributions), but
+#  this entails a risk that all distances in the random samples aren't
+#  covered, e.g. if a gene is expressed in a limited region. (Since
+#  those spots are sampled from the whole array.)
+# NB - what is an appropriate number of bins?
 maxdist <- max(distrib(colnames(testdata)))
 breaks <- seq(0,maxdist, by=(maxdist/200))
 
 
-# Ta för varje gen ut spotsen med nollskilt värde ("values"), beräkna
-#  avstånden. eukl innehåller avstånden mellan spotsen med uttryck
-#  för varje gen, följt av nollor. Skapa först en matris för countsen
-#  av (antal bins) kolumner, n+2 rader ty en fördelning för
-#  de faktiska avstånden, n slumpmässiga spotfördelningar, och en
-#  medel av dessa. n = antal slumpmässiga
-#  spotuppsättningar som ska dras, slideseq använder 1000.
-#  Skapa eukl - vektor för avstånden, av längd (x-1)+(x-2)+...+1 där
-#  x är antalet spots med uttryck av genen i fråga.
-# p - för att spara undan p-värdet för varje gen.
+# For each gene, take the spots with non-zero counts ("values"), 
+#  calculate the distances. eukl contains the distances between the 
+#  spots withexpression of each gene, followed by zeroes. First create
+#  a matrix for the counts of (number of bins) columns, (n+2) rows - 
+#  one distribution for the true distances, n random distributions, and
+#  one mean random distribution. n= the number of random sets of spots 
+#  to sample, SlideSeq uses n = 1000.
+# Create eukl - a vector for the distances, of length (x-1)+(x-2)+...+1
+#  where x is the number of spots with expression of the gene in 
+#  question. p - to save the p value for each gene.
 n <- 1000
 p <- vector(mode="numeric", length=nrow(testdata))
 
@@ -118,11 +118,11 @@ for (i in 1:nrow(testdata)){
   counts_matrix <- matrix(0, nrow = (n + 2), ncol = (length(breaks)-1))
   counts_matrix[1,] <- eukdistr$counts
   
-  # Samma antal avstånd för varje slumpmässig uppsättning spots för
-  #  varje gen som för de faktiska spotsen där den uttrycks, n
-  #  slumpmässiga avståndsfördelningar (i form av antalet counts i 
-  #  varje bin) sparas på rad 2 till n+1 i count_matrix.
-  # I eukl_for_rand sparas de faktiska avstånden. 
+  # The same number of distances for each random set of spots for each
+  #  gene as for the actual spots where it is expressed. n random 
+  #  distance distributions are saved (as the number of counts in each
+  #  bin) on row 2 to n+1 in count_matrix. In eukl_for_rand the actual
+  #  distances are saved.
   
   eukl_for_rand <- matrix(0, nrow = n, ncol = length(eukl))
   
@@ -133,35 +133,37 @@ for (i in 1:nrow(testdata)){
                                   plot=FALSE)$counts
   }
   
-  # Tolkar elementwise mean som att ta medel av countsen per bin  
+  # Interpreting element-wise mean as taking the mean of the counts in 
+  #  each bin
   counts_matrix[(n+2),] <- colMeans(counts_matrix[(2:(n+1)),])
   
-  # Elementvis differens för avståndsfördelningen för GOI och 
-  #  medelfördelningen, samt slumpfördelningarna och medel.
+  # Element-wise difference for the distance distribution of the GOI
+  #  and the mean distribution, as well as for the random distributions
+  #  and the mean.
   diff_matr <- matrix(0, (n+1), ncol(counts_matrix))
   diff_matr[1,] <- counts_matrix[1,]-counts_matrix[n+2,]
   for (m in 1:n){
     diff_matr[m+1,] <- counts_matrix[m+1,]-counts_matrix[n+2,]
   }
   
-  # Ta beloppet av dessa, och summera över varje rad --> L1-normen
-  #  för avståndet mellan den verkliga fördelningen (element 1) 
-  #  respektive de slumpade fördelningarna (element 2-(n+1)) och
-  #  den genomsnittliga slumpfördelningen. n/2 bins, tex
+  # Take the absolute value of these, and sum over each row --> the
+  #  L1-norm of the distance between the true distribution (element 1)
+  #  and the random distributions (element 2 to (n+1)), respectively, 
+  #  and the mean random distribution. 
   abs_diff <- abs(diff_matr)
   L1_norms <- rowSums(abs_diff)
   #hist(L1_norms[2:(n+1)], n/2, xlim=c(min(L1_norms),max(L1_norms)))
   #abline(v=L1_norms[1], col="red")
   
-  # p-värdet är som minst p<1/n, där n är antalet slumpmässiga 
-  #  spotuppsättningar. Generellt är p=(slumpmässiga prover med
-  #  L1>L1(sanna provet))/(totalt antal slumpmässiga prover). Om
-  #  täljaren är 0 kan dock bara sägas att p<1/n. I suppl. till 
-  #  artikeln säger de att p=(slumpm prover med L1<L1(sanna))/(totalt
-  #  antal slumpmässiga), men detta motsäger resonemanget de för,
-  #  och exemplet i figur S10. För p<1/n: sparar i nuläget värdet
-  #  som p=1/(10n), för hanteringens skull. Kanske fixa detta på  
-  #  något snyggare sätt?
+  # The smallest possible p value is p < 1/n, with n being the number
+  #  of random sets of spots.In general, p = (no. random samples with
+  #  L1>L1(true sample))/(total no. random samples). If the numerator
+  #  is 0, it can however only be said that p < 1/n. In the supplementary
+  #  material to the paper it is said that p = (no. random samples with
+  #  L1<L1(true sample))/(total no. random samples), but this contradicts
+  #  the authors' argument, and the example in figure S10. For p < 1/n:
+  #  for the time being, the value is saved as p = (1/10n), to set it apart
+  #  from actual p values of 1/n. Can this be done in a nicer way?
   
   over_L1 <- L1_norms[which(L1_norms>L1_norms[1])]
   if (length(over_L1)!=0){
